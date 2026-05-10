9d31543be634a6b75c3567272e8d575a30cb7725
tkacdominik18 <tkacdominik18@gmail.com>
2026-05-10 23:19:01 +0200

    Update shopify.js

--- api/shopify.js
+++ api/shopify.js
@@ -1,10 +1,9 @@
 export default async function handler(req, res) {
   const shop = "uhq06t-kz.myshopify.com";
   const token = process.env.SHOPIFY_ACCESS_TOKEN;
-  const { from, to } = req.query;
 
   try {
-    const url = ⁠ https://${shop}/admin/api/2026-04/orders.json?status=any&created_at_min=${from}&created_at_max=${to}&limit=250 ⁠;
+    const url = "https://" + shop + "/admin/api/2026-04/orders.json?status=any&limit=250";
     const response = await fetch(url, {
       headers: { "X-Shopify-Access-Token": token }
     });
